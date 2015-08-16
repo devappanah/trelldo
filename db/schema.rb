8f69e352ed945a028bd376b1648bc09150538712
Dev Appanah <devappanah@gmail.com>
2015-08-16 10:39:41 +0700

    Added tasks scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150813072408) do
+ActiveRecord::Schema.define(version: 20150816033240) do
+
+  create_table "tasks", force: :cascade do |t|
+    t.text     "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
